e6d90f2fd11e5a618c86f8d0e2078ba5df88c080
devden7 <devdenn7@gmail.com>
2024-01-02 15:43:50 +0700

    adjustment font-style

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <p className="h1-semibold font-causten">
-        Trendy is the last stage before tacky.
-      </p>
-      <p className="h1-bold font-coresans">
-        Trendy is the last stage before tacky.
-      </p>
+      <p className="h1-semibold">Trendy is the last stage before tacky.</p>
+      <p className="h1-bold">Trendy is the last stage before tacky.</p>
     </div>
   );
 }
